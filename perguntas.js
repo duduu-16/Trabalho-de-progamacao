a4254c74d967610e8d19faa2a0f12cbba0c5a628
duduu-16 <eduardo.pagliarini@escola.pr.gov.br>
2024-11-17 19:48:54 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,36 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'Malu.mp4'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+    criaCartao(
+    'Lingua inglesa',
+    'Como se diz OI em inglês?',
+    'Oi em ingles é HI (RAI)'
+) 
+
+criaCartao(
+    'Matematica',
+    'Qual é a fórmula de Bhaskara?',
+    '(-b±√(b²-4ac))/(2a)'
+) 
+
+criaCartao(
+    'História',
+    'Quem foi Fernando Henrique Cardoso?',
+    'Ele foi um sociólogo e ex-presidente do Brasil.'
+) 
+
